5c2c66efa9e354b319635bf18a6c7ad7ea2d0bcb
Yakoiukugura <56473303+yakoiukugura@users.noreply.github.com>
2023-02-27 21:23:02 +0200

    Add files via upload

---
+++ Flying Cards/Flying Cards/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace Flying_Cards
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.panel = new System.Windows.Forms.PictureBox();
+            this.pictureShow = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureShow)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.label1.Location = new System.Drawing.Point(254, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(496, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Which card have you seen before?";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = global::Flying_Cards.Properties.Resources._1_1;
+            this.pictureBox1.Location = new System.Drawing.Point(340, 141);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(132, 162);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Image = global::Flying_Cards.Properties.Resources._1_2;
+            this.pictureBox2.Location = new System.Drawing.Point(515, 141);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(132, 162);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Image = global::Flying_Cards.Properties.Resources._1_3;
+            this.pictureBox3.Location = new System.Drawing.Point(340, 340);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(132, 162);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox4.Image = global::Flying_Cards.Properties.Resources._1_4;
+            this.pictureBox4.Location = new System.Drawing.Point(515, 340);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(12);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(132, 162);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // panel
+            // 
+            this.panel.Image = global::Flying_Cards.Properties.Resources.background;
+            this.panel.Location = new System.Drawing.Point(0, 0);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(989, 559);
+            this.panel.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.panel.TabIndex = 5;
+            this.panel.TabStop = false;
+            // 
+            // pictureShow
+            // 
+            this.pictureShow.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureShow.Location = new System.Drawing.Point(357, 141);
+            this.pictureShow.Name = "pictureShow";
+            this.pictureShow.Size = new System.Drawing.Size(202, 232);
+            this.pictureShow.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureShow.TabIndex = 6;
+            this.pictureShow.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::Flying_Cards.Properties.Resources.background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(988, 559);
+            this.Controls.Add(this.pictureShow);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Flying Cards";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureShow)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox panel;
+        private System.Windows.Forms.PictureBox pictureShow;
+    }
+}
+
---
+++ Flying Cards/Flying Cards/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flying_Cards
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int i = 1;
+        int[] choices = new int[11];
+        string path = "C:\\Users\\TheBoss\\OneDrive\\Desktop\\Atestate\\Flying Cards\\Flying Cards\\Resources\\";
+
+        public void wait(int milliseconds)
+        {
+            var timer1 = new System.Windows.Forms.Timer();
+            if (milliseconds == 0 || milliseconds < 0) return;
+
+            timer1.Interval = milliseconds;
+            timer1.Enabled = true;
+            timer1.Start();
+
+            timer1.Tick += (s, e) =>
+            {
+                timer1.Enabled = false;
+                timer1.Stop();
+            };
+
+            while (timer1.Enabled)
+            {
+                Application.DoEvents();
+            }
+        }
+
+        private void getGoodChoices()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                choices[i] = new Random().Next(1, 5);
+                pictureShow.Image = Image.FromFile(path + i + "-" + choices[i] + ".png");
+                wait(3000);
+            }
+            pictureShow.Enabled = false;
+            pictureShow.Visible = false;
+            panel.Enabled = false;
+            panel.Visible = false;
+        }
+
+        private void nextLevel()
+        {
+            i++;
+            if (i > 10)
+            {
+                MessageBox.Show("You Win!");
+                this.Close();
+            }
+            else
+            {
+                pictureBox1.Image = Image.FromFile(path + i + "-1.png");
+                pictureBox2.Image = Image.FromFile(path + i + "-2.png");
+                pictureBox3.Image = Image.FromFile(path + i + "-3.png");
+                pictureBox4.Image = Image.FromFile(path + i + "-4.png");
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            PictureBox pic = sender as PictureBox;
+            if (pic == pictureBox1)
+            {
+                if (choices[i] == 1)
+                    pic.Image = Image.FromFile(path + "tick.png");
+                else
+                    pic.Image = Image.FromFile(path + "x.png");
+            }
+            else if (pic == pictureBox2)
+            {
+                if (choices[i] == 2)
+                    pic.Image = Image.FromFile(path + "tick.png");
+                else
+                    pic.Image = Image.FromFile(path + "x.png");
+            }
+            else if (pic == pictureBox3)
+            {
+                if (choices[i] == 3)
+                    pic.Image = Image.FromFile(path + "tick.png");
+                else
+                    pic.Image = Image.FromFile(path + "x.png");
+            }
+            else if (pic == pictureBox4)
+            {
+                if (choices[i] == 4)
+                    pic.Image = Image.FromFile(path + "tick.png");
+                else
+                    pic.Image = Image.FromFile(path + "x.png");
+            }
+            wait(1000);
+            nextLevel();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            getGoodChoices();
+        }
+    }
+}
