d3858d5c969755c9937df1bda808ffec913de206
Yakoiukugura <56473303+yakoiukugura@users.noreply.github.com>
2023-05-09 01:58:44 +0300

    Add files via upload

---
+++ Flying Cards/Flying Cards/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿
+namespace Flying_Cards
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.panel = new System.Windows.Forms.PictureBox();
+            this.pictureShow = new System.Windows.Forms.PictureBox();
+            this.menu = new System.Windows.Forms.Panel();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.quit_button = new System.Windows.Forms.Button();
+            this.start_button = new System.Windows.Forms.Button();
+            this.title = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.txtTime = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureShow)).BeginInit();
+            this.menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.label1.Location = new System.Drawing.Point(254, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(496, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Which card have you seen before?";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = global::Flying_Cards.Properties.Resources._1_1;
+            this.pictureBox1.Location = new System.Drawing.Point(340, 141);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(132, 162);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Image = global::Flying_Cards.Properties.Resources._1_2;
+            this.pictureBox2.Location = new System.Drawing.Point(515, 141);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(132, 162);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Image = global::Flying_Cards.Properties.Resources._1_3;
+            this.pictureBox3.Location = new System.Drawing.Point(340, 340);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(132, 162);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox4.Image = global::Flying_Cards.Properties.Resources._1_4;
+            this.pictureBox4.Location = new System.Drawing.Point(515, 340);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(12);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(132, 162);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // panel
+            // 
+            this.panel.Image = global::Flying_Cards.Properties.Resources.background;
+            this.panel.Location = new System.Drawing.Point(0, 0);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(998, 559);
+            this.panel.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.panel.TabIndex = 5;
+            this.panel.TabStop = false;
+            // 
+            // pictureShow
+            // 
+            this.pictureShow.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureShow.Location = new System.Drawing.Point(357, 141);
+            this.pictureShow.Name = "pictureShow";
+            this.pictureShow.Size = new System.Drawing.Size(202, 232);
+            this.pictureShow.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureShow.TabIndex = 6;
+            this.pictureShow.TabStop = false;
+            // 
+            // menu
+            // 
+            this.menu.BackColor = System.Drawing.SystemColors.Highlight;
+            this.menu.BackgroundImage = global::Flying_Cards.Properties.Resources.background;
+            this.menu.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.menu.Controls.Add(this.txtScore);
+            this.menu.Controls.Add(this.quit_button);
+            this.menu.Controls.Add(this.start_button);
+            this.menu.Controls.Add(this.title);
+            this.menu.Location = new System.Drawing.Point(1, 0);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(1013, 615);
+            this.menu.TabIndex = 7;
+            // 
+            // txtScore
+            // 
+            this.txtScore.BackColor = System.Drawing.Color.Transparent;
+            this.txtScore.Font = new System.Drawing.Font("Palatino Linotype", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtScore.Location = new System.Drawing.Point(16, 195);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(964, 54);
+            this.txtScore.TabIndex = 3;
+            this.txtScore.Text = "Score: ";
+            this.txtScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.txtScore.Visible = false;
+            // 
+            // quit_button
+            // 
+            this.quit_button.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.quit_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quit_button.Location = new System.Drawing.Point(410, 403);
+            this.quit_button.Name = "quit_button";
+            this.quit_button.Size = new System.Drawing.Size(157, 65);
+            this.quit_button.TabIndex = 2;
+            this.quit_button.Text = "Quit";
+            this.quit_button.UseVisualStyleBackColor = true;
+            this.quit_button.Click += new System.EventHandler(this.quit_button_Click);
+            // 
+            // start_button
+            // 
+            this.start_button.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.start_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start_button.Location = new System.Drawing.Point(410, 293);
+            this.start_button.Name = "start_button";
+            this.start_button.Size = new System.Drawing.Size(157, 65);
+            this.start_button.TabIndex = 1;
+            this.start_button.Text = "Start";
+            this.start_button.UseVisualStyleBackColor = true;
+            this.start_button.Click += new System.EventHandler(this.start_button_Click);
+            // 
+            // title
+            // 
+            this.title.BackColor = System.Drawing.Color.Transparent;
+            this.title.Font = new System.Drawing.Font("Palatino Linotype", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.title.Location = new System.Drawing.Point(22, 108);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(964, 87);
+            this.title.TabIndex = 0;
+            this.title.Text = "Flying Cards";
+            this.title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // txtTime
+            // 
+            this.txtTime.BackColor = System.Drawing.Color.Transparent;
+            this.txtTime.Font = new System.Drawing.Font("Palatino Linotype", 48F, System.Drawing.FontStyle.Bold);
+            this.txtTime.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtTime.Location = new System.Drawing.Point(745, 221);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(206, 179);
+            this.txtTime.TabIndex = 8;
+            this.txtTime.Text = "5";
+            this.txtTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::Flying_Cards.Properties.Resources.background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(988, 559);
+            this.Controls.Add(this.menu);
+            this.Controls.Add(this.pictureShow);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTime);
+            this.Name = "Form1";
+            this.Text = "Flying Cards";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureShow)).EndInit();
+            this.menu.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox panel;
+        private System.Windows.Forms.PictureBox pictureShow;
+        private System.Windows.Forms.Panel menu;
+        private System.Windows.Forms.Button quit_button;
+        private System.Windows.Forms.Button start_button;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label txtTime;
+    }
+}
+
---
+++ Flying Cards/Flying Cards/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flying_Cards
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Declara variabilele de nivel curent si punctaj curent
+        int CURRENT_LEVEL;
+        int SCORE;
+
+        //Declara locatia imaginilor si un vector pentru a stoca alegerile corecte
+        string PATH = "C:\\Users\\TheBoss\\OneDrive\\Desktop\\Atestate\\Flying Cards\\Flying Cards\\Resources\\";
+        int[] choices = new int[11];
+
+        //Declara un timer pentru timpul ramas si timpul ramas
+        int timeLeft = 5;
+
+        //Initializarea jocului
+        private void start_button_Click(object sender, EventArgs e)
+        {
+            //Seteaza nivelul curent si punctajul la inceput
+            CURRENT_LEVEL = 0;
+            SCORE = 10;
+
+            //Ascunde meniul de pornire
+            menu.Enabled = false;
+            menu.Visible = false;
+
+            //Ascunde titlul jocului
+            title.Enabled = false;
+            title.Visible = false;
+
+            //Ascunde butonul de pornire joc
+            start_button.Enabled = false;
+            start_button.Visible = false;
+
+            //Ascunde butonul de iesire din joc
+            quit_button.Enabled = false;
+            quit_button.Visible = false;
+
+            //Seteaza nivelul urmator si genereaza alegerile corecte
+            nextLevel();
+            getGoodChoices();
+        }
+
+        //Functie pentru butonul de iesire din joc
+        private void quit_button_Click(object sender, EventArgs e)
+        {
+            //Afiseaza un mesaj pentru confirmarea iesirii din joc
+            if (MessageBox.Show("Are you sure you want to quit?", "Quit Game", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                this.Close();
+        }
+
+        //Functie pentru a genera alegerile corecte
+        private void getGoodChoices()
+        {
+            //Afiseaza o imagine si seteaza panoul pentru alegere
+            pictureShow.Enabled = true;
+            pictureShow.Visible = true;
+            panel.Enabled = true;
+            panel.Visible = true;
+
+            //Genereaza alegerile corecte
+            for (int i = 1; i <= 10; i++)
+            {
+                choices[i] = new Random().Next(1, 5);
+                pictureShow.Image = Image.FromFile(PATH + i + "-" + choices[i] + ".png");
+                wait(500);
+            }
+            //Ascunde imaginea si panoul de alegere
+            pictureShow.Enabled = false;
+            pictureShow.Visible = false;
+            panel.Enabled = false;
+            panel.Visible = false;
+            //Porneste timer-ul pentru timpul ramas si afiseaza timpul ramas
+            timer1.Enabled = true;
+            txtTime.Visible = true;
+            txtTime.Enabled = true;
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            // Aceasta functie este apelata atunci cand unul dintre cele patru PictureBox-uri este apasat.
+            // Functia verifica daca imaginea selectata este corecta sau nu si modifica imaginea corespunzatoare cu marcajul "tick" sau "x".
+            // Daca imaginea selectata este gresita, scorul este decrementat cu 1.
+            // Apoi se asteapta 1 secunda si se trece la urmatorul nivel prin apelarea functiei nextLevel().
+            PictureBox pic = sender as PictureBox;
+            if (pic == pictureBox1 && choices[CURRENT_LEVEL] == 1)
+                pic.Image = Image.FromFile(PATH + "tick.png");
+            else if (pic == pictureBox2 && choices[CURRENT_LEVEL] == 2)
+                pic.Image = Image.FromFile(PATH + "tick.png");
+            else if (pic == pictureBox3 && choices[CURRENT_LEVEL] == 3)
+                pic.Image = Image.FromFile(PATH + "tick.png");
+            else if (pic == pictureBox4 && choices[CURRENT_LEVEL] == 4)
+                pic.Image = Image.FromFile(PATH + "tick.png");
+            else
+            {
+                pic.Image = Image.FromFile(PATH + "x.png");
+                SCORE--;
+            }
+            wait(1000);
+            nextLevel();
+        }
+
+        private void nextLevel()
+        {
+            // Aceasta functie incrementeaza numarul nivelului curent si verifica daca jocul s-a terminat sau nu.
+            // Daca jocul s-a terminat, functia va apela functia gameOver() pentru a afisa scorul si a permite jucatorului sa reinceapa jocul.
+            // Daca jocul nu s-a terminat, se actualizeaza imaginile PictureBox-urilor cu cele corespunzatoare noului nivel.
+            timeLeft = 5;
+            CURRENT_LEVEL++;
+            if (CURRENT_LEVEL > 10)
+            {
+                gameOver();
+            }
+            else
+            {
+                pictureBox1.Image = Image.FromFile(PATH + CURRENT_LEVEL + "-1.png");
+                pictureBox2.Image = Image.FromFile(PATH + CURRENT_LEVEL + "-2.png");
+                pictureBox3.Image = Image.FromFile(PATH + CURRENT_LEVEL + "-3.png");
+                pictureBox4.Image = Image.FromFile(PATH + CURRENT_LEVEL + "-4.png");
+            }
+        }
+
+        private void gameOver()
+        {
+            // Aceasta functie afiseaza un mesaj care indica daca jucatorul a castigat sau a pierdut jocul si scorul obtinut.
+            // Apoi, se afiseaza butoanele "Play Again" si "Quit" pentru a permite jucatorului sa reinceapa jocul sau sa il inchida.
+            // Timerul care masoara timpul ramas pentru fiecare nivel este dezactivat si ascuns, iar label-ul care afiseaza scorul jucatorului este afisat.
+
+            menu.Enabled = true;
+            menu.Visible = true;
+
+            if (SCORE > 4)
+                title.Text = "You Win!";
+            else
+                title.Text = "You Lose!";
+
+            title.Enabled = true;
+            title.Visible = true;
+
+            start_button.Text = "Play Again";
+            start_button.Enabled = true;
+            start_button.Visible = true;
+
+            quit_button.Enabled = true;
+            quit_button.Visible = true;
+
+            txtScore.Visible = true;
+            txtScore.Text = "Score: " + SCORE;
+
+            timer1.Enabled = false;
+            txtTime.Visible = false;
+            txtTime.Enabled = false;
+            txtTime.Text = "5";
+        }
+
+        // Functia "wait" asteapta un anumit numar de milisecunde inainte de a continua executia
+        // https://stackoverflow.com/questions/10458118/wait-one-second-in-running-program
+        public void wait(int milliseconds)
+        {
+            var timer1 = new System.Windows.Forms.Timer();
+            if (milliseconds == 0 || milliseconds < 0) return;
+
+            timer1.Interval = milliseconds;
+            timer1.Enabled = true;
+            timer1.Start();
+
+            timer1.Tick += (s, e) =>
+            {
+                timer1.Enabled = false;
+                timer1.Stop();
+            };
+
+            while (timer1.Enabled)
+            {
+                Application.DoEvents();
+            }
+        }
+
+        // Aceasta functie este apelata la fiecare tick al timer-ului (la fiecare secunda).
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Se actualizeaza textul din textbox-ul care indica timpul ramas.
+            txtTime.Text = timeLeft.ToString();
+            // Daca timpul a expirat
+            if (timeLeft == 0)
+            {
+                // Se scade un punct din scor si se trece la urmatorul nivel.
+                SCORE--;
+                nextLevel();
+            }
+            else
+                // Daca timpul nu a expirat, se scade o secunda din timpul ramas.
+                timeLeft--;
+        }
+    }
+}
